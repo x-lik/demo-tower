95bd9c924623d8372599aa08ee707c03f1edcd01
hunzsig <mzyhaohaoren@qq.com>
2024-09-21 16:50:10 +0800

    update

--- assets/sound.lua
+++ assets/sound.lua
@@ -1,10 +1,7 @@
 
 --【 音效 】
 
-assets_vcm("Sound\\Interface\\AutoCastButtonClick1.wav", "war3_AutoCastButtonClick1")
-assets_vcm("Sound\\Interface\\CreepAggroWhat1.wav", "war3_CreepAggroWhat1")
 assets_vcm("Sound\\Interface\\Error.wav", "war3_Error")
-assets_vcm("Sound\\Interface\\GoodJob.wav", "war3_GoodJob")
 assets_vcm("Sound\\Interface\\HeroDropItem1.wav", "war3_HeroDropItem1")
 assets_vcm("Sound\\Interface\\MapPing.wav", "war3_MapPing")
 assets_vcm("Sound\\Interface\\MouseClick1.wav", "war3_MouseClick1")
