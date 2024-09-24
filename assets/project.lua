b0b0d9243782aba878b3ad2091426ae0852ab4b0
hunzsig <mzyhaohaoren@qq.com>
2024-09-24 18:10:02 +0800

    feat(UI): *

--- assets/project.lua
+++ assets/project.lua
@@ -8,7 +8,7 @@ assets_model("buff/Echo", "Echo")
 
 assets_vwp("metal_medium_slice") --武器声音，每种3段
 
-assets_ui("lik_info")
-assets_ui("lik_plate")
-assets_ui("lik_buff")
-assets_ui("lik_debug")
+assets_ui("xlik_info")
+assets_ui("xlik_plate")
+assets_ui("xlik_buff")
+assets_ui("xlik_debug")
